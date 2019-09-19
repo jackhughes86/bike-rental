66458de5ffefde76c4a3c6a235f47a15bc743092
keithlem <keithlem9@gmail.com>
2019-09-19 12:36:26 -0400

    Updates

---
+++ precache-manifest.4d5c7598716e88fe8c3502dc559aa4c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c9c0b229b2b35855e9be9ace6454d3",
+    "url": "/bike-rental/index.html"
+  },
+  {
+    "revision": "242b5f2e030b8e6bba08",
+    "url": "/bike-rental/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b80950b08be8028bc8c4",
+    "url": "/bike-rental/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "242b5f2e030b8e6bba08",
+    "url": "/bike-rental/static/js/2.1e83f467.chunk.js"
+  },
+  {
+    "revision": "b80950b08be8028bc8c4",
+    "url": "/bike-rental/static/js/main.29cc38a2.chunk.js"
+  },
+  {
+    "revision": "b81b1c1b4dedd63145ad",
+    "url": "/bike-rental/static/js/runtime~main.a112275c.js"
+  }
+]);
